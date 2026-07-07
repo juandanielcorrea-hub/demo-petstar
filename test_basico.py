e703239403444ae2d05723167c9016b670120dfb
Juan-Daniel-Correa-Cardona <danicorreacar@gmail.com>
2026-07-07 10:33:47 -0600

    Caliz 2

--- test_basico.py
+++ test_basico.py
@@ -1,3 +1,5 @@
 def test_pipeline_funciona():
     """Prueba básica para asegurar que pytest tiene algo que recolectar y el CI pasa."""
-    assert True
+    print("Hola mundo")
+
+test_pipeline_funciona()
